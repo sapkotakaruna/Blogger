238f1d134fba1f9f8c9a95123f06f16f872a9dd7
sapkotakaruna <sapkotakaru@gmail.com>
2022-12-18 12:27:51 +0545

    Blogger

---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Crud;
+use Illuminate\Http\Request;
+
+class CrudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['rows'] = Crud::select('id','title','slug','rank','status')->orderBy('id','desc');
+        return view('crud.index',compact('data'));
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Crud $crud)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Crud $crud)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Crud $crud)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Crud  $crud
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Crud $crud)
+    {
+        //
+    }
+}
---
+++ app/Models/Crud.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Crud extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'photo',
+        'description',
+        'rank',
+        'status'
+
+    ];
+}
